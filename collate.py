aa53a2eeb8acb249c5101dc271b3b55f8a54a931
George K. Thiruvathukal <thiruvathukal@gmail.com>
2012-11-17 22:17:56 -0600

    collate.py

--- collate.py
+++ collate.py
@@ -1,54 +1,88 @@
 import sys
 import re
 import nltk
-
-from difflib import *
-
-EOLMARKER = '==\n' 
+import optparse
+import difflib
+import os.path
+import optparse
 
 def tokenize(text):
    return [token + '\n' for token in nltk.word_tokenize(text)]
+
+def get_spans(text, tokens):
+   i = -1
+   for t in tokens:
+      i = text.find(i+1, t)
+      yield (i, i+len(t))
 
 def dump_tokens(filename, tokens):
    with open(filename, 'w') as out:
       out.write('\n'.join(tokens))
       out.write('\n')
 
-import os.path
-text1 = sys.argv[1]
-text2 = sys.argv[2]
-(basename1, ext) = os.path.splitext(text1)
-(basename2, ext) = os.path.splitext(text2)
-tokens1 = basename1 + '.tokens'
-tokens2 = basename2 + '.tokens'
+def get_command_line():
+   usage = """usage: %prog [options] master-file compare-file"""
+   parser = optparse.OptionParser(usage)
+   parser.add_option("-t", "--tokens", action="store_true", default=False,
+      help='Store hte tokens for debugging purposes')
+   parser.add_option("-l", "--lines", type="int", default=0,
+      help="Set number of context lines (default 0). Don't change this unless you know why.")
+   parser.add_option("-o", "--output", type="string", default=None,
+      help='Set the output filename. Default is diff-<MASTER>-<COMPARE>.out')
+   options, args = parser.parse_args()
+   if len(args) == 0:
+      parser.print_help()
+      sys.exit(1)
+   return options, args
 
-f1 = open(text1)
-f2 = open(text2)
+def main():
+   options, args = get_command_line()
+   if len(args) != 2:
+      parser.error("need to specify both a master and compare file")
 
-content1 = f1.readlines()
-content2 = f2.readlines()
+   master_file, compare_file = args # as specified in the usage string
 
-print("Processing %s" % basename1)
-s1 = []
-for line in content1:
-   s1 = s1 + tokenize(line) + [ EOLMARKER ]
+   text1 = master_file
+   text2 = compare_file
+   (basename1, ext) = os.path.splitext(text1)
+   (basename2, ext) = os.path.splitext(text2)
+   tokens1 = basename1 + '.tokens'
+   tokens2 = basename2 + '.tokens'
 
-dump_tokens(tokens1, s1)
- 
-print("Processing %s" % basename2)
-s2 = []
-for line in content2:
-   s2 = s2 + tokenize(line) + [ EOLMARKER ]
+   f1 = open(text1)
+   f2 = open(text2)
 
-dump_tokens(tokens2, s2)
+   content1 = f1.readlines()
+   content2 = f2.readlines()
 
-(head, tail1) = os.path.split(basename1)
-(head, tail2) = os.path.split(basename2)
-compare_filename = 'diff-%s-%s.out' % (tail1, tail2) 
+   print("Processing %s" % basename1)
+   s1 = []
+   for line in content1:
+      s1 = s1 + tokenize(line) 
 
-print("Writing %s" % compare_filename)
+   if options.tokens:
+      dump_tokens(tokens1, s1)
+    
+   print("Processing %s" % basename2)
+   s2 = []
+   for line in content2:
+      s2 = s2 + tokenize(line)
 
-with open(compare_filename, "w") as out:
-   for line in context_diff(s1, s2, fromfile=text1, tofile=text2, n=0):
-      out.write(line)
+   if options.tokens:
+      dump_tokens(tokens2, s2)
 
+   if options.output == None:
+      (head, tail1) = os.path.split(basename1)
+      (head, tail2) = os.path.split(basename2)
+      compare_filename = 'diff-%s-%s.out' % (tail1, tail2)
+   else:
+      compare_filename = options.output
+
+   print("Writing %s" % compare_filename)
+
+   with open(compare_filename, "w") as out:
+      for line in difflib.context_diff(s1, s2, fromfile=text1, tofile=text2, n=0):
+         out.write(line)
+
+if __name__ == '__main__':
+   main()
